f8c215810ade59176ecbb0f9b333d75fb63d8fc6
Kevin RRivera <krivera1015@gmail.com>
2018-08-31 14:57:34 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+let index = 0
+
+
 function init() {
-  // your code here
+  const body = document.body
+  body.addEventListener('keydown', function(e) {
+
+    const directionCode = e.key
+
+    if (directionCode === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        console.log(alert('Sweet Konami! 100 tacos for you!'))
+        index = 0
+      }
+    } else{
+      index = 0
+    }
+  })
 }
